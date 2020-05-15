36238719adafce52807b0f8aa26a9ef9a2defbc6
Celestin Cordos <celestin.cordos@ensta-paris.fr>
2020-05-15 17:46:24 +0200

    transforming 2.0 in code with classes
    also correcting adding some 'smarter' commands like np.mean () and 'apply'

--- Project/Seeance 2/Consommation 2.1.py
+++ Project/Seeance 2/Consommation 2.1.py
@@ -25,6 +25,9 @@ from sklearn.linear_model import LinearRegression
 
 import itertools
 
+
+class demand ():
+    
 def split_fs(f, p):
     f = f >> mutate(NW = -X.injection + X.withdrawal)
     f['FSW1'] = f.apply (create_fsw1, axis=1)
@@ -34,9 +37,9 @@ def split_fs(f, p):
     f['NW_b'] = f.apply (create_binary_nw, axis=1)
     f = (f >> inner_join(p, by='gasDayStartedOn') >> drop(0, 2, 3, 4, 5, 6, 7, 8, 9, 10)).dropna()
     return f
-
 #fonction initialisation
-seuil =45
+    seuil =45
+    
 def initialisation_data (s = 'storage_data.xlsx', p = 'price_data.csv'):
     storage_data = pd.read_excel(s, sheet_name=None)
     price_data = pd.read_csv(p)
@@ -167,105 +170,97 @@ def random_forest(x,y):
 
 
 for k, v in storage_data.items():
-    if k != 'SF - UGS Stassfurt':
-        dataFrame = storage_data [k]
-        dataFrame.dropna()
-        
-        del dataFrame ['status'] 
-        del dataFrame ['trend']
-        n = dataFrame.shape [0]
-        
-        netWithdrawal = [0]*n
-        FSW1 = [0]*n
-        FSW2 = [0]*n
-        netWithdrawalBinary = [0]*n
-        netWithdrawalLagged = [0]* n
-        
-        for i in range (n):
-            netWithdrawal [i] = dataFrame['withdrawal'][i] - dataFrame['injection'][i]
-            FSW1[i] = max (dataFrame['full'][i] - seuil, 0)
-            FSW2[i] = max (- dataFrame['full'][i] + seuil, 0)
-            if netWithdrawal[i] > 0:
-                netWithdrawalBinary [i] = 1
-            if i >  0:
-                netWithdrawalLagged [i -1 ] = netWithdrawal [i]
-        
-        
-        
-        
-        netWithdrawal [i] = 0
+    dataFrame = storage_data [k]
+    dataFrame.dropna()
+    
+    del dataFrame ['status'] 
+    del dataFrame ['trend']
+    n = dataFrame.shape [0]
+    
+    netWithdrawal = [0]*n
+    FSW1 = [0]*n
+    FSW2 = [0]*n
+    netWithdrawalBinary = [0]*n
+    netWithdrawalLagged = [0]* n
+    
+    for i in range (n):
+        netWithdrawal [i] = dataFrame['withdrawal'][i] - dataFrame['injection'][i]
         FSW1[i] = max (dataFrame['full'][i] - seuil, 0)
         FSW2[i] = max (- dataFrame['full'][i] + seuil, 0)
-        
-        dataFrame ['Net Withdrawal binary'] = netWithdrawalBinary
-        dataFrame ['Net Withdrawal'] = netWithdrawal
-        dataFrame ['Net Withdrawal Lagged'] = netWithdrawalLagged
-        dataFrame ['FSW1'] = FSW1
-        dataFrame ['FSW2'] = FSW2
-        
-        dataFrame.dropna (inplace = True )
-        
-        storage_data[k] = dataFrame
-    #    dfply.join.inner_join(dataFrame, price_data)
+        if netWithdrawal[i] > 0:
+            netWithdrawalBinary [i] = 1
+        if i >  0:
+            netWithdrawalLagged [i -1 ] = netWithdrawal [i]
+    
+    
+    
+    
+    netWithdrawal [i] = 0
+    FSW1[i] = max (dataFrame['full'][i] - seuil, 0)
+    FSW2[i] = max (- dataFrame['full'][i] + seuil, 0)
+    
+    dataFrame ['Net Withdrawal binary'] = netWithdrawalBinary
+    dataFrame ['Net Withdrawal'] = netWithdrawal
+    dataFrame ['Net Withdrawal Lagged'] = netWithdrawalLagged
+    dataFrame ['FSW1'] = FSW1
+    dataFrame ['FSW2'] = FSW2
+    
+    dataFrame.dropna (inplace = True )
+    
+    storage_data[k] = dataFrame
+#    dfply.join.inner_join(dataFrame, price_data)
 
 #deuxieme partie a partir d'ici
-        feature_cols = ['Net Withdrawal Lagged', 'FSW1', 'FSW2']
-        x = np.array(dataFrame[feature_cols]) # Features
-        y = np.array(dataFrame['Net Withdrawal binary']) # Target variable
-        model1[k]=Logistic_Regression(x,y)
-        model2[k]=random_forest(x,y)
-        
-        #ajouter fonction pour comprarer et choisir le meilleur
-        
-        
-        
+    feature_cols = ['Net Withdrawal Lagged', 'FSW1', 'FSW2']
+    x = np.array(dataFrame[feature_cols]) # Features
+    y = np.array(dataFrame['Net Withdrawal binary']) # Target variable
+    model1[k]=Logistic_Regression(x,y)
+    model2[k]=random_forest(x,y)
+    
+    #ajouter fonction pour comprarer et choisir le meilleur
+    
+    
+    
 ################REGRESSION###############
-        X = dataFrame[feature_cols] # Features
-        #prendre les nw >0 : storage = storage[storage.NW_b != 0]
-        y = dataFrame['Net Withdrawal'] # Target variable
+    X = dataFrame[feature_cols] # Features
+    storage = storage[storage.NW_b != 0]
+    y = dataFrame['Net Withdrawal'] # Target variable
 
-       
-        
-        
-        #plt.figure(figsize=(15,10))
-        #plt.tight_layout()
-        #seabornInstance.distplot(dataset['quality'])
-        
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-        
-        regressor = LinearRegression()  
-        l_reg = regressor.fit(X_train, y_train)
-        
-        coeff_df = pd.DataFrame(regressor.coef_, X.columns, columns=['Coefficient'])  
-        coeff_df
-        
-        y_pred = regressor.predict(X_test)
-        
-        df = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
-        df1 = df.head(25)
-        
-        df1.plot(kind='bar',figsize=(10,8))
-        plt.grid(which='major', linestyle='-', linewidth='0.5', color='green')
-        plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
-        plt.show()
-        
-        #np.mean ()
-        RMSE = np.sqrt(metrics.mean_squared_error(y_test, y_pred))
-        
-        n = len (y_test)
-        averageValueConsumption = 0
-        minValue = min (y_test)
-        for i in range (n):
-            averageValueConsumption += y_test.values[i]
-            
-        
-        averageValueConsumption /= n
-        maxValueConsumption = np.max (y_test)
-        ANRMSE = RMSE/averageValueConsumption
-        NRMSE = RMSE/(maxValueConsumption - minValue)
-        r2 = metrics.r2_score(y_test, y_pred)
-        d_regression = {'r2': r2, 'rmse': RMSE, 'nrmse': NRMSE, 'anrmse': ANRMSE, 'l_reg':l_reg }#pas complet des choses à comprendre et à completer
-        dict_regression[k] = d_regression
+   
+    
+    
+    #plt.figure(figsize=(15,10))
+    #plt.tight_layout()
+    #seabornInstance.distplot(dataset['quality'])
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+    
+    regressor = LinearRegression()  
+    l_reg = regressor.fit(X_train, y_train)
+    
+    coeff_df = pd.DataFrame(regressor.coef_, X.columns, columns=['Coefficient'])  
+    coeff_df
+    
+    y_pred = regressor.predict(X_test)
+    
+    df = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
+    df1 = df.head(25)
+    
+    df1.plot(kind='bar',figsize=(10,8))
+    plt.grid(which='major', linestyle='-', linewidth='0.5', color='green')
+    plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+    plt.show()
+    
+    #np.mean ()
+    RMSE = np.sqrt(metrics.mean_squared_error(y_test, y_pred))   
+    averageValueConsumption = np.mean (y_test)
+    maxValueConsumption = np.max (y_test)
+    ANRMSE = RMSE/averageValueConsumption
+    NRMSE = RMSE/(maxValueConsumption - minValue)
+    r2 = metrics.r2_score(y_test, y_pred)
+    corr = pearsonr(y_test, y_pred)[0]
+    d_regression = {'r2': r2, 'rmse': RMSE, 'nrmse': NRMSE, 'anrmse': ANRMSE, 'corr': corr, 'l_reg':l_reg }
+    dict_regression[k] = d_regression
         
         
 
