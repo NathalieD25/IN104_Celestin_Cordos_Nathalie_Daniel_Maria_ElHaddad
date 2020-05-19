75ec29cdcc2a2bb5909613268aea137f2129cc99
NathalieD25 <62898847+NathalieD25@users.noreply.github.com>
2020-05-19 22:45:25 +0300

    Update balance.py

--- Project/BALANCE/balance.py
+++ Project/BALANCE/balance.py
@@ -1,12 +1,12 @@
 
 import dfply
 import DEMAND
-#import supply
-
+import supply
+import os
+from openpyxl.workbook import Workbook
 def market_decision(DF):
-
     decision=[]
-    for i in range (0,len(DF)):
+    for i in range (0,len(balance)):
         if DF['Supply'][i]>DF['Demand'][i]:
             decision.append("SELL")
 
@@ -14,7 +14,7 @@ def market_decision(DF):
         if DF['Supply'][i]<DF['Demand'][i]:
             #print ("On %s : decision is BUY" %(DF['Date'][i]))
             decision.append("BUY")
-        else:
+        if DF['Supply'][i]==DF['Demand'][i]:
             #print ("On %s : decision is FLAT" %(DF['Date'][i]))
             decision.append("FLAT")
 
@@ -22,14 +22,13 @@ def market_decision(DF):
 
 def main():
     DF_demand=df
-
-    DF_supply= pd.read_csv('Book1.csv', sep = ',') ####just to test the function: the DF_supply has to be a dataFRAME
-
+    DF_supply=Supply
+    DF_supply.rename(columns={"gasDayStartedOn":"Date"},inplace=True)
     DF_demand['Date'] = pd.to_datetime(DF_demand['Date'])
     DF_supply['Date'] = pd.to_datetime(DF_supply['Date'])
 
     balance=DF_demand>> inner_join(DF_supply,by='Date') ##inner joining the 2 date frames
-
+    c=market_decision(balance)
     balance["Decision"]=market_decision(balance)
-
+    balance.to_excel("Balance.xlsx")
     print(balance)
