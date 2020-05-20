94fc33e6b02c56501ce7e2a184d452ee1d5e7b23
Celestin Cordos <celestin.cordos@ensta-paris.fr>
2020-05-20 17:54:18 +0200

    Update test_supply.py

--- Project/Seeance 2/test_supply.py
+++ Project/Seeance 2/test_supply.py
@@ -15,11 +15,11 @@ class Test(unittest.TestCase) :
    
    def test_method_comparison (self) :
       A= Consommation.Classification(storage_data,{"SF - UGS Rehden": {} ,"SF - UGS Kraak": {},"SF - UGS Stassfut" :{},"SF - UGS Harsefeld" :{},"SF - UGS Breitburnn" : {}, "SF - UGS Epe Uniper H-Gas" : {}, "SF - UGS Eschenfelden" : {}, "SF - UGS Inzeham-West ": {}, "SF - UGS Bierwang" : {}, "SF - UGS Jemgum H (EWE)" : {}, "SF - UGS Peckensen" : {}, " SF - UGS Peckensen " : {}, " SF  -UGS Etzel ESE (Uniper Ener) " : {} },{"SF - UGS Rehden": {} ,"SF - UGS Kraak": {},"SF - UGS Stassfut" :{},"SF - UGS Harsefeld" :{},"SF - UGS Breitburnn" : {}, "SF - UGS Epe Uniper H-Gas" : {}, "SF - UGS Eschenfelden" : {}, "SF - UGS Inzeham-West ": {}, "SF - UGS Bierwang" : {}, "SF - UGS Jemgum H (EWE)" : {}, "SF - UGS Peckensen" : {}, " SF - UGS Peckensen " : {}, " SF  -UGS Etzel ESE (Uniper Ener) " : {} })
-      self.assertEqual(type(methode_comparison(A,d1,d2)), str)
+      self.assertEqual(type(methode_comparison(d1,d2)), str)
 
-   def test_regression (self) :
-      A= Consommation.Regression(Consommation.storage_data,dict(),dict())
-      self.assertEqual(type(y),dataFrame)
+#   def test_regression (self) :
+#      A= Consommation.Regression()
+#      self.assertEqual(type(y),dataFrame)
 
 
 if __name__ == '__main__':
