b50782f72c48412188b63f27a695c18f3ff70288
Celestin Cordos <celestin.cordos@ensta-paris.fr>
2020-05-21 14:55:14 +0200

    creating tests

--- Project/BALANCE/test_supply.py
+++ Project/BALANCE/test_supply.py
@@ -1,25 +1,45 @@
-import Consommation
+import supply
 import unittest
+import pandas
+import numpy as np
 
-class Test(unittest.TestCase) :
+class test_Classification (unittest.TestCase) :
 
-   def Test_Logistic_Regression(self) :
-      A= Consommation.Classification()
-      self.assertEqual(type(Consommation.Logistic_Regression(A,2,3)[0]), dict)
-      
+    regression = supply.Classification ()
+    items = regression.Storages.items()
+    stockage = items [0][1]
+    feature_cols = ['NW_Lagged', 'FSW1', 'FSW2']
+    x = stockage[feature_cols] # Features
+    y = np.zeros (feature_cols.shape[0])
+    
+    def test_Supplies (self):
+        storage_data = self.stockage
+        self.assertIsInstance (storage_data, dict) #teste si c'est de la bonne forme
+        for k,v in storage_data.items():
+            self.assertIn ('NW', v.columns)
+            self.assertIn ('NW_Lagged', v.columns)
+            self.assertIn ('FSW1', v.columns)
+            self.assertIn ('FSW2', v.columns)
+            self.assertIn ('NW_b', v.columns)
+    
+    def test_random_forest (self):
+        d, lr = self.regression.random_forest (self.x,self.y)
+        self.assertIsInstance (d, dict)
+        
+    def test_Logistic_Regression (self):
+        d, lr = self.regression.Logistic_Regression (self.x,self.y)
+        self.assertIsInstance (d, dict)
+        
+#    def test_compare_model ():
 
-   def test_Random_forest (self) :
-      A= Consommation.Regression()
-      self.assertEqual(type(A.random_forest(A,x)[0]),dict)
-      
-   
-   def test_method_comparison (self) :
-      A= Consommation.Classification()
-      self.assertEqual(type(methode_comparison(d1,d2)), str)
+        
+        
+    
 
-#   def test_regression (self) :
-#      A= Consommation.Regression()
-#      self.assertEqual(type(y),dataFrame)
+
+
+
+
 
 
 if __name__ == '__main__':
