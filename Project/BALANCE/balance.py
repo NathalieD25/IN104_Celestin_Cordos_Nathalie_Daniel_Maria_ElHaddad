ed47a7be757c8e88d50de5db70e256249f3fdd85
Celestin Cordos <celestin.cordos@ensta-paris.fr>
2020-05-22 15:08:56 +0200

    changes

--- Project/BALANCE/balance.py
+++ Project/BALANCE/balance.py
@@ -7,7 +7,7 @@ from sklearn.metrics import mean_squared_error
 import scipy
 
 
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, recall_score, precision_score, roc_auc_score
 
 
 
@@ -21,21 +21,21 @@ def market_decision(DF):
     DF['Decision_real'] = 0
     for i in range (0,len(DF)):
         if DF['Supply'][i]>DF['Demand'][i]:
-            DF['Decision'][i] = 1
+            DF['Decision'][i] = 'SELL'
 
             #print ("On %s : decision is SELL" %(DF['Date'][i]))
         if DF['Supply'][i]<DF['Demand'][i]:
             #print ("On %s : decision is BUY" %(DF['Date'][i]))
-            DF['Decision'][i] =0
+            DF['Decision'][i] ='BUY'
         if DF['Supply'][i]==DF['Demand'][i]:
             #print ("On %s : decision is FLAT" %(DF['Date'][i]))
-            DF['Decision'][i] =2
+            DF['Decision'][i] ='FLAT'
         if DF['Supply_real'][i]>DF['Demand_real'][i]:
-            DF['Decision_real'][i] =1
+            DF['Decision_real'][i] ='SELL'
         if DF['Supply_real'][i]<DF['Demand_real'][i]:
-            DF['Decision_real'][i]=0
+            DF['Decision_real'][i]='BUY'
         if DF['Supply_real'][i]==DF['Demand_real'][i]:
-            DF['Decision_real'][i] = 2
+            DF['Decision_real'][i] = 'FLAT'
         
 
 
@@ -72,12 +72,9 @@ def main():
 
     balance=DF_demand>> dfply.inner_join(DF_supply,by='Date') ##inner joining the 2 date frames
     market_decision(balance)
-#    balance["Decision"]=market_decision(balance)[0]
-#    balance["Decision_real"]=market_decision(balance)[1]
+
     balance = balance[(balance.T != 0).all()]
-#    balance.replace(0, np.nan, inplace = True)
-#    balance.dropna(how='all', axis=0, inplace = True)
-#    balance.replace(np.nan, 0, inplace = True)
+
     
     
     balance.to_csv('final_balance.csv',index=True)
@@ -85,8 +82,24 @@ def main():
     print(balance)
     print ('demand metrics : ', d_demand)
     print ('supply metrics : ',  d_supply)
-    cm=confusion_matrix(balance ['Decision'].values, balance['Decision_real'].values)
-    print (cm)
+    for i in range (len (balance)):
+        if balance ['Decision'].values[i] == 'SELL':
+            balance['Decision'].values[i] = 1
+        if balance ['Decision'].values[i] == 'BUY':
+            balance['Decision'].values[i] = 0
+        if balance ['Decision'].values[i] == 'FLAT':
+            balance['Decision'].values[i] = 2
+        if balance ['Decision_real'].values[i] == 'SELL':
+            balance['Decision_real'].values[i] = 1
+        if balance ['Decision_real'].values[i] == 'BUY':
+            balance['Decision_real'].values[i] = 0
+        if balance ['Decision_real'].values[i] == 'FLAT':
+            balance['Decision_real'].values[i] = 2
+            
+    y_test, y_pred = list(balance['Decision_real']),  list(balance['Decision'])    
+    cm = confusion_matrix (y_test, y_pred)
+    d_decision = {'recall': recall_score(y_test, y_pred), "neg_recall": cm[1,1]/(cm[0,1] + cm[1,1]),"confusion": cm,"precision": precision_score(y_test, y_pred), "neg_precision":cm[1,1]/cm.sum(axis=1)[1], "roc": roc_auc_score(y_test,y_pred)}
+    print ('decision metrics : ', d_decision)
     
    
     
