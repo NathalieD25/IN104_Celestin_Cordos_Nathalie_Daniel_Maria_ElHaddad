c6c34c057124d59692c7c33eb37619d3a67b6469
Celestin Cordos <celestin.cordos@ensta-paris.fr>
2020-05-22 15:14:03 +0200

    Update balance.py

--- Project/BALANCE/balance.py
+++ Project/BALANCE/balance.py
@@ -57,31 +57,11 @@ def metrics (balance):
     NRMSE = RMSE_s/(maxValueConsumption_s - minValueConsumption_s)
     corr = scipy.stats.pearsonr(balance['Supply_real'],balance['Supply'])[0]
     d_supply = {'rmse': RMSE_d, 'nrmse': NRMSE, 'anrmse': ANRMSE, 'corr': corr }
-    
-    
+
     
     return d_demand, d_supply
-    
 
-def main():
-    DF_demand=DEMAND.df
-    DF_supply=supply.Supply
-    DF_supply.rename(columns={"gasDayStartedOn":"Date"},inplace=True)
-    DF_demand['Date'] = pd.to_datetime(DF_demand['Date'])
-    DF_supply['Date'] = pd.to_datetime(DF_supply['Date'])
-
-    balance=DF_demand>> dfply.inner_join(DF_supply,by='Date') ##inner joining the 2 date frames
-    market_decision(balance)
-
-    balance = balance[(balance.T != 0).all()]
-
-    
-    
-    balance.to_csv('final_balance.csv',index=True)
-    d_demand, d_supply = metrics (balance)
-    print(balance)
-    print ('demand metrics : ', d_demand)
-    print ('supply metrics : ',  d_supply)
+def metrics_decision (balance):
     for i in range (len (balance)):
         if balance ['Decision'].values[i] == 'SELL':
             balance['Decision'].values[i] = 1
@@ -99,6 +79,28 @@ def main():
     y_test, y_pred = list(balance['Decision_real']),  list(balance['Decision'])    
     cm = confusion_matrix (y_test, y_pred)
     d_decision = {'recall': recall_score(y_test, y_pred), "neg_recall": cm[1,1]/(cm[0,1] + cm[1,1]),"confusion": cm,"precision": precision_score(y_test, y_pred), "neg_precision":cm[1,1]/cm.sum(axis=1)[1], "roc": roc_auc_score(y_test,y_pred)}
+    return d_decision
+
+def main():
+    DF_demand=DEMAND.df
+    DF_supply=supply.Supply
+    DF_supply.rename(columns={"gasDayStartedOn":"Date"},inplace=True)
+    DF_demand['Date'] = pd.to_datetime(DF_demand['Date'])
+    DF_supply['Date'] = pd.to_datetime(DF_supply['Date'])
+
+    balance=DF_demand>> dfply.inner_join(DF_supply,by='Date') ##inner joining the 2 date frames
+    market_decision(balance)
+
+    balance = balance[(balance.T != 0).all()]
+
+    
+    
+    balance.to_csv('final_balance.csv',index=True)
+    d_demand, d_supply = metrics (balance)
+    d_decision = metrics_decision (balance)
+    print(balance)
+    print ('demand metrics : ', d_demand)
+    print ('supply metrics : ',  d_supply)
     print ('decision metrics : ', d_decision)
     
    
